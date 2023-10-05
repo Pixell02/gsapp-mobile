93e1c48f58b2632c83029617e3b35aa0028eb32c
Pixel <nikodems1308@gmail.com>
2023-10-06 01:05:58 +0200

    save file with auto formatter

--- App.tsx
+++ App.tsx
@@ -1,24 +1,23 @@
-
-import { StyleSheet, Text, View } from "react-native";
-import "expo-dev-client";
-import "react-native-gesture-handler";
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import "expo-dev-client";
 import { useFonts } from "expo-font";
-import { AuthContextProvider } from "./context/AuthContext";
-import { LanguageContext, LanguageProvider } from "./context/LanguageContext";
-import * as Localization from 'expo-localization';
-import { useContext, useEffect, useState } from "react";
+import * as Localization from "expo-localization";
+import { useEffect, useState } from "react";
+import { View } from "react-native";
+import "react-native-gesture-handler";
 import ScreenContainer from "./ScreenContainer";
-import AsyncStorage from '@react-native-async-storage/async-storage';
-const Stack = createStackNavigator();
+import { AuthContextProvider } from "./context/AuthContext";
+import { LanguageProvider } from "./context/LanguageContext";
+
 
 export default function App() {
-  const [language, setLanguage] = useState('');
+  const [language, setLanguage] = useState("");
   useEffect(() => {
-    const currentLocale = Localization.locale.split('-')[0];
-    setLanguage(currentLocale || 'en');
+    const currentLocale = Localization.locale.split("-")[0];
+    setLanguage(currentLocale || "en");
   }, []);
+
+
   const [fontsLoaded] = useFonts({
     "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
     "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
@@ -27,23 +26,16 @@ export default function App() {
   if (!fontsLoaded) {
     return <View />;
   }
-  
+
   return (
     <AuthContextProvider>
       <LanguageProvider>
-      <NavigationContainer>
-        <ScreenContainer /> 
-      </NavigationContainer>
+        <NavigationContainer>
+          <ScreenContainer />
+        </NavigationContainer>
       </LanguageProvider>
     </AuthContextProvider>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+
