0c8a9d9b75e4ab7228a29b18afd6b3cd574d2948
Pixel <nikodems1308@gmail.com>
2023-10-06 20:47:16 +0200

    component that doesn't work

---
+++ screens/CreatorScreen/component/EditPanel/SingleElements/BackgroundImageButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const BackgroundImageButton = ({webViewRef, coords, filters, i}) => {
+
+  return (
+    <View style={{marginTop: 10}}>
+        <Text>Nie działa V</Text>
+    </View>
+  )
+}
+
+export default BackgroundImageButton
