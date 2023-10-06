5f7620225f338b8e725e290c0f59a53d3292938b
Pixel <nikodems1308@gmail.com>
2023-10-07 00:15:34 +0200

    add types

--- screens/PlayersScreen/hooks/useTeamOption.tsx
+++ screens/PlayersScreen/hooks/useTeamOption.tsx
@@ -1,15 +1,24 @@
 
 
-import React, { useEffect, useState } from 'react'
-import { useCollection } from '../../../hooks/useCollection';
+import { useEffect, useState } from 'react';
 import { useAuthContext } from '../../../hooks/useAuthContext';
+import { useCollection } from '../../../hooks/useCollection';
+
+interface TeamProps {
+  firstName: string;
+  secondName: string;
+}
+interface optionProps {
+  label: string;
+  value: string;
+}
 
 const useTeamOption = () => {
   const { user } = useAuthContext();
-  const [teamOption, setTeamOption] = useState([]);
+  const [teamOption, setTeamOption] = useState<optionProps[] | []>([]);
   const { documents: Teams } = useCollection("Teams", ["uid", "==", user.uid]);
   useEffect(() => {
-    const option = Teams?.map((team, i) => ({
+    const option = Teams?.map((team: TeamProps, i: string) => ({
       id: i,
       label: team.firstName + " " + team.secondName,
       value: team.firstName + " " + team.secondName,
