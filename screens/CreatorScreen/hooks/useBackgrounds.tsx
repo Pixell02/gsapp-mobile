7965d0c67c8f0ad6e6cd0256d06f0bdbc9aeaccf
Pixel <nikodems1308@gmail.com>
2023-10-06 22:41:11 +0200

    add types

--- screens/CreatorScreen/hooks/useBackgrounds.tsx
+++ screens/CreatorScreen/hooks/useBackgrounds.tsx
@@ -1,76 +1,88 @@
-import { useContext, useEffect, useState } from 'react';
-import { useCollection } from '../../../hooks/useCollection';
-import { ThemeOptionContext } from '../context/themeOptionContext';
+import { useEffect, useState } from "react";
+import { useCollection } from "../../../hooks/useCollection";
+import useFetch from "../../../hooks/useFetch";
+import useThemeOption from "./useThemeOption";
 interface Background {
   uuid: string;
   src: string;
   color: string;
 }
 
+interface backgroundProps {
+  uuid: string;
+  uid: string;
+  name: string;
+  src: string;
+  color: string
+}
 
+interface posterInfoProps {
+  posterId: string;
+  name: string;
+  src: string;
+}
 
 const useBackgrounds = (uid: string) => {
-  
-  const { documents: mainBackgrounds } = useCollection("piecesOfPoster", ["uuid", "==", uid])
-  const { documents: individualBackgrounds } = useCollection("yourCatalog", ["uuid", "==", uid])
-  const { setPosterInfo } = useContext(ThemeOptionContext);
+  const { documents: mainBackgrounds } = useCollection("piecesOfPoster", [
+    "uuid",
+    "==",
+    uid,
+  ]);
+  const { documents: individualBackgrounds } = useCollection("yourCatalog", [
+    "uuid",
+    "==",
+    uid,
+  ]);
+  const { setPosterInfo, setSelectedTheme } = useThemeOption();
   const [backgrounds, setBackgrounds] = useState<Background[]>([]);
   const [selectedBackground, setSelectedBackground] = useState(null);
-  const [dataURL, setDataURL] = useState<string>("");
+  const { image: dataURL } = useFetch(selectedBackground?.src);
+  const { image: additionalLayer } = useFetch(
+    selectedBackground?.additionalLayer
+  );
+
+  useEffect(() => {
+    if (selectedBackground) {
+      setSelectedTheme(selectedBackground.color);
+    }
+  }, [selectedBackground]);
+
+  useEffect(() => {
+    if (backgrounds?.length > 0) {
+      setSelectedBackground(backgrounds[0]);
+    }
+  }, [backgrounds]);
+
   useEffect(() => {
     if (mainBackgrounds?.length > 0) {
-      setBackgrounds(mainBackgrounds);
-      mainBackgrounds?.forEach((item: any) => {
-        console.log(item.uid)
-        if(item.uid){
-          setPosterInfo(prev => ({
+      setBackgrounds(mainBackgrounds[0]);
+      mainBackgrounds?.forEach((item: backgroundProps) => {
+        if (item.uid) {
+          setPosterInfo((prev: posterInfoProps) => ({
             ...prev,
             posterId: item.uid,
             name: item.name,
-            src: item.src
-          }))
+            src: item.src,
+          }));
         }
       });
     } else {
-      setBackgrounds(individualBackgrounds);
-      individualBackgrounds?.forEach((item: any) => {
-        if(item.uuid) {
-          setPosterInfo(prev => ({
+      setBackgrounds(individualBackgrounds[0]);
+      individualBackgrounds?.forEach((item: backgroundProps) => {
+        if (item.uuid) {
+          setPosterInfo((prev: posterInfoProps) => ({
             ...prev,
             posterId: item.uid,
             name: item.color,
-            src: item.src
-          }))
+            src: item.src,
+          }));
         }
       });
     }
-  }, [mainBackgrounds, individualBackgrounds])
+  }, [mainBackgrounds, individualBackgrounds]);
 
-  
+  return { backgrounds, selectedBackground, setSelectedBackground, dataURL, additionalLayer };
+};
 
-  const handleFetchBackground = (background: string) => {
-    
-    fetch(background.split("...")[0])
-      .then((res) => res.blob())
-      .then((blob) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(blob);
-        reader.onloadend = () => {
-          setDataURL(reader.result as string);
-          setSelectedBackground(background)
-        }
-        
-      })
-      .catch((err) => {
-        console.log(err)
-        setDataURL("");
-      })
-  }
-  
-  
+export default useBackgrounds;
 
-
-  return { backgrounds, selectedBackground, dataURL, handleFetchBackground}
-}
-
-export default useBackgrounds
