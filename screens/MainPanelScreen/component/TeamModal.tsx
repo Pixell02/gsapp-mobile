cb7760823a358843e47fdbd475e736c4894a4d28
Pixel <nikodems1308@gmail.com>
2023-10-07 00:17:11 +0200

    reduce code

--- screens/MainPanelScreen/component/TeamModal.tsx
+++ screens/MainPanelScreen/component/TeamModal.tsx
@@ -1,60 +1,19 @@
-import React, { useState } from "react";
+import React from "react";
 import AddTeam from "./AddTeam";
-import EditTeam from "./EditTeam";
 import AddTrainer from "./AddTrainer";
+import EditTeam from "./EditTeam";
 import EditTrainer from "./EditTrainer";
+import useModalContextProvider from "./hooks/useModalContextProvider";
 
-interface props {
-  isOpen: boolean;
-  setIsOpen: () => void;
-  teamData: any;
-  setTeamData: any;
-  isEditOpen: boolean;
-  setIsEditOpen: () => void;
-  isTrainerOpen: boolean;
-  setIsTrainerOpen: any;
-  trainerData: any;
-  setTrainerData: any;
-  isEditTrainerOpen: boolean;
-  setIsEditTrainerOpen: any;
-}
+export default function TeamModal() {
+  const { isModalOpen } = useModalContextProvider();
 
-export default function TeamModal({
-  isOpen,
-  setIsOpen,
-  teamData,
-  setTeamData,
-  isEditOpen,
-  setIsEditOpen,
-  isTrainerOpen,
-  setIsTrainerOpen,
-  trainerData,
-  setTrainerData,
-  isEditTrainerOpen,
-  setIsEditTrainerOpen
-}: props) {
   return (
     <>
-      {isOpen && <AddTeam isOpen={isOpen} setIsOpen={setIsOpen} teamData={teamData} setTeamData={setTeamData} />}
-      {isEditOpen && (
-        <EditTeam isEditOpen={isEditOpen} setIsEditOpen={setIsEditOpen} teamData={teamData} setTeamData={setTeamData} />
-      )}
-      {isTrainerOpen && (
-        <AddTrainer
-          isTrainerOpen={isTrainerOpen}
-          trainerData={trainerData}
-          setTrainerData={setTrainerData}
-          setIsTrainerOpen={setIsTrainerOpen}
-        />
-      )}
-      {isEditTrainerOpen && (
-        <EditTrainer 
-          trainerData={trainerData}
-          setTrainerData={setTrainerData}
-          isEditTrainerOpen={isEditTrainerOpen}
-          setIsEditTrainerOpen={setIsEditTrainerOpen}
-        />
-      )}
+      {isModalOpen === 1 && <AddTeam />}
+      {isModalOpen === 2 && <EditTeam />}
+      {isModalOpen === 3 && <AddTrainer />}
+      {isModalOpen === 4 && <EditTrainer />}
     </>
   );
 }
