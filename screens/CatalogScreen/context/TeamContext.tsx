3c7f039879848b1f86d203c87aa25ea6dc9b4cd6
Pixel <nikodems1308@gmail.com>
2023-10-06 11:26:46 +0200

    add concat, save file with auto formatter

--- screens/CatalogScreen/context/TeamContext.tsx
+++ screens/CatalogScreen/context/TeamContext.tsx
@@ -1,49 +1,58 @@
 import { createContext, useEffect, useState } from "react";
 import { useAuthContext } from "../../../hooks/useAuthContext";
 import { useCollection } from "../../../hooks/useCollection";
-
-
+import useTeamCollection from "../../../hooks/useTeamCollection";
 
 export const TeamContext = createContext(null);
 
+interface teamProps {
+  sport: string
+}
 
 export const TeamProvider = ({ children }) => {
-
   const { user } = useAuthContext();
   const { documents: Teams } = useCollection("Teams", ["uid", "==", user.uid]);
-  
-  const [sportKeys, setSportKeys] = useState(null);
-  const [sportOptions, setSportOptions] = useState(null);
+  const { documents: LicensedTeams } = useTeamCollection("Teams");
+  const [sportKeys, setSportKeys] = useState([]);
+  const [sportOptions, setSportOptions] = useState([]);
   const [selectedSportKeys, setSelectedSportKeys] = useState(null);
 
   useEffect(() => {
-    if (Teams) {
+    if (Teams || LicensedTeams) {
       const uniqueSportKeys = Array.from(
-        new Set(Teams.map((team) => team.sport))
+        new Set(
+          (Teams || []).concat(LicensedTeams || []).map((team: teamProps) => team.sport)
+        )
       );
       setSportKeys(uniqueSportKeys);
     }
-  }, [Teams]);
-  
+  }, [Teams, LicensedTeams]);
 
   useEffect(() => {
-    const options = sportKeys?.map((sport) => ({
+    const options = sportKeys.map((sport) => ({
       label: sport,
-      value: sport
+      value: sport,
     }));
     setSportOptions(options);
-  }, [sportKeys])
-  
+  }, [sportKeys]);
+
   useEffect(() => {
-    if(sportOptions)
+    if (sportOptions.length > 0) {
       setSelectedSportKeys(sportOptions[0]?.value);
-  },[sportOptions])
-  
+    }
+  }, [sportOptions]);
 
   return (
-    <TeamContext.Provider value={{sportKeys, setSportKeys, sportOptions, selectedSportKeys, setSelectedSportKeys}}>
-     {children} 
+    <TeamContext.Provider
+      value={{
+        sportKeys,
+        setSportKeys,
+        sportOptions,
+        selectedSportKeys,
+        setSelectedSportKeys,
+      }}
+    >
+      {children}
     </TeamContext.Provider>
-)
-
-}
+  );
+};
