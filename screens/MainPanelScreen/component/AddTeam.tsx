f3936ac530b7e0c70eb6c9749ea588567cd51baa
Pixel <nikodems1308@gmail.com>
2023-10-10 10:24:37 +0200

    auto format

--- screens/MainPanelScreen/component/AddTeam.tsx
+++ screens/MainPanelScreen/component/AddTeam.tsx
@@ -8,6 +8,7 @@ import Title from "../../../components/Title";
 import useAddImage from "../../../hooks/useAddImage";
 import useCustomPanResponder from "../../../hooks/useCustomPanResponder";
 import useLanguageContext from "../../../hooks/useLanguageContext";
+import { teamDataProps } from "../context/DataContext";
 import translate from "../locales/translate.json";
 import useDataContextProvider from "./hooks/useDataContextProvider";
 import useHandleTeamEvents from "./hooks/useHandleTeamEvents";
@@ -27,25 +28,25 @@ const AddTeam = () => {
     <View {...panResponder.panHandlers}>
       <Modal animationType="slide" visible={isModalOpen === 1} onRequestClose={() => setIsModalOpen(0)}>
         <View style={styles.modalContent}>
-          <Title name={(translate.addTeam[language] || translate.addTeam["en"])}/>
+          <Title name={translate.addTeam[language] || translate.addTeam["en"]} />
           <View style={styles.inputCenter}>
             <InputData
-              name={(translate.firstTeamName[language] || translate.firstTeamName["en"])}
+              name={translate.firstTeamName[language] || translate.firstTeamName["en"]}
               text={teamData.firstName}
-              onChangeText={(value) => setTeamData((prev) => ({ ...prev, firstName: value.trim() }))}
+              onChangeText={(value) => setTeamData((prev: teamDataProps) => ({ ...prev, firstName: value.trim() }))}
             />
             <InputData
-              name={(translate.secondTeamName[language] || translate.secondTeamName["en"])}
+              name={translate.secondTeamName[language] || translate.secondTeamName["en"]}
               text={teamData.secondName}
-              onChangeText={(value) => setTeamData((prev) => ({ ...prev, secondName: value.trim() }))}
-            /> 
-            <Text style={{width:"100%", fontFamily:"Poppins-SemiBold"}}>{(translate.sport[language] || translate.sport["en"])}</Text>
-            <View
-              style={styles.pickerContainer}
-            >
+              onChangeText={(value) => setTeamData((prev: teamDataProps) => ({ ...prev, secondName: value.trim() }))}
+            />
+            <Text style={{ width: "100%", fontFamily: "Poppins-SemiBold" }}>
+              {translate.sport[language] || translate.sport["en"]}
+            </Text>
+            <View style={styles.pickerContainer}>
               <Picker
                 selectedValue={teamData.sport}
-                onValueChange={(itemValue) => setTeamData((prev) => ({ ...prev, sport: itemValue }))}
+                onValueChange={(itemValue) => setTeamData((prev: teamDataProps) => ({ ...prev, sport: itemValue }))}
               >
                 {sportOptions.map((item) => (
                   <Picker.Item key={item.label} label={item.label} value={item.value} />
@@ -53,11 +54,14 @@ const AddTeam = () => {
               </Picker>
             </View>
             <View style={styles.margin}>
-              <RoundedButton text={(translate.addImage[language] || translate.addImage["en"])} onPress={handleAddPhoto} />
+              <RoundedButton text={translate.addImage[language] || translate.addImage["en"]} onPress={handleAddPhoto} />
             </View>
             <PreviewBlock preview={preview} setImageUri={setImageUri} />
-            <View style={{width: "100%"}}>
-              <RoundedButton text={(translate.save[language] || translate.save["en"])} onPress={() => handleSave(teamData, preview)} />
+            <View style={{ width: "100%" }}>
+              <RoundedButton
+                text={translate.save[language] || translate.save["en"]}
+                onPress={() => handleSave(teamData, preview)}
+              />
             </View>
           </View>
         </View>
