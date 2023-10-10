81a255f0f097890f71595727b9c8e3367dad9637
Pixel <nikodems1308@gmail.com>
2023-10-10 12:52:13 +0200

    add useEffects to add data

--- screens/PlayersScreen/component/AddSquadPreset/PlayersToCheck.tsx
+++ screens/PlayersScreen/component/AddSquadPreset/PlayersToCheck.tsx
@@ -1,27 +1,52 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { View } from "react-native";
 import { CheckBox } from "react-native-elements";
 import SelectPicker from "../../../../components/SelectPicker";
 import useLanguageContext from "../../../../hooks/useLanguageContext";
 import useSelectedTeamContext from "../../../CreatorScreen/hooks/useSelectedTeamContext";
+import { playerProps, selectedPlayerProps, squadPresetProps } from "../../context/DataContext";
+import useDataContext from "../../hooks/useDataContext";
 import translate from "../../locales/translate.json";
 import usePlayerOption from "./hooks/usePlayerOption";
 
-const PlayersToCheck = ({ goalkeeper, setGoalkeeper, capitan, setCapitan }) => {
-  const { Players, selectedPlayers, handlePlayerChecked } = useSelectedTeamContext();
+const PlayersToCheck = () => {
+  const { Players, selectedPlayers, handlePlayerChecked, selectedReserve } = useSelectedTeamContext();
   const option = usePlayerOption(selectedPlayers);
+  const { squadData, setSquadData } = useDataContext();
   const { language } = useLanguageContext();
+  
+
+  useEffect(() => {
+    setSquadData((prev: squadPresetProps) => ({
+      ...prev,
+      squadPlayers: selectedPlayers
+    }))
+  },[selectedPlayers])
+
+  useEffect(() => {
+    setSquadData((prev: squadPresetProps) => ({
+      ...prev,
+      reservePlayers: selectedReserve
+    }))
+  },[selectedReserve])
+
+
+  const handleValueChange = (value: string, className: string) => {
+    setSquadData((prev: selectedPlayerProps[]) => ({
+      ...prev,
+      [className]: value
+    }))
+  }
 
   return (
     <View>
-      {Players &&
-        Players.map((player, i) => (
+      {Players?.map((player: playerProps, i: string) => (
           <View key={i} style={{ flexDirection: "row", alignItems: "center" }}>
             <View style={{ width: "100%", alignItems: "center" }}>
               <View style={{ width: "80%" }}>
                 <CheckBox
                   checked={selectedPlayers.some(
-                    (selectedPlayer: any) =>
+                    (selectedPlayer: selectedPlayerProps) =>
                       selectedPlayer.firstName === player.firstName &&
                       selectedPlayer.secondName === player.secondName &&
                       selectedPlayer.number === player.number
@@ -37,17 +62,17 @@ const PlayersToCheck = ({ goalkeeper, setGoalkeeper, capitan, setCapitan }) => {
         <View style={{ width: "80%" }}>
           <SelectPicker
             options={option}
-            selectedValue={capitan}
+            selectedValue={squadData.capitan}
             name={translate.capitan[language || "en"]}
-            onValueChange={(value) => setCapitan(value)}
+            onValueChange={(value) => handleValueChange(value, "capitan")}
           />
         </View>
         <View style={{ width: "80%", marginTop: 10 }}>
           <SelectPicker
             options={option}
-            selectedValue={goalkeeper}
+            selectedValue={squadData.goalKeeper}
             name={translate.goalkeeper[language || "en"]}
-            onValueChange={(value) => setGoalkeeper(value)}
+            onValueChange={(value) => handleValueChange(value, "goalkeeper")}
           />
         </View>
       </View>
