a2e0974e6ede42d292e82acba759ac0a774d7d71
Pixel <nikodems1308@gmail.com>
2023-10-10 12:52:26 +0200

    add useEffect to add data

--- screens/PlayersScreen/component/PlayersMainContent/SquadPreset.tsx
+++ screens/PlayersScreen/component/PlayersMainContent/SquadPreset.tsx
@@ -7,26 +7,34 @@ import Title from "../../../../components/Title";
 import { useAuthContext } from "../../../../hooks/useAuthContext";
 import { useCollection } from "../../../../hooks/useCollection";
 import useLanguageContext from "../../../../hooks/useLanguageContext";
+import useModalContextProvider from "../../../MainPanelScreen/component/hooks/useModalContextProvider";
+import useDataContext from "../../hooks/useDataContext";
 import translate from "../../locales/translate.json";
 
-const SquadPreset = ({ setSquadData, setIsOpen }) => {
+interface squadPresetProps {
+  presetName: string;
+}
+
+const SquadPreset = () => {
   const { language } = useLanguageContext();
   const { user } = useAuthContext();
+  const { setIsModalOpen } = useModalContextProvider();
+  const { setSquadData } = useDataContext();
   const { documents: squadPreset } = useCollection("squadPreset", ["uid", "==", user.uid]);
 
-  const handlePress = (item) => {
-    setSquadData(item)
-    setIsOpen(4)
-  }
+  const handlePress = (item: squadPresetProps) => {
+    setSquadData((prev: squadPresetProps) => ({...prev, ...item}));
+    setIsModalOpen(4);
+  };
 
   return (
     <View>
       <Title name={translate.squadPreset[language || "en"]} />
       <View style={{ width: "30%", marginLeft: 10 }}>
-        <RoundedButton text={translate.addPreset[language || "en"]} onPress={() => setIsOpen(3)} />
+        <RoundedButton text={translate.addPreset[language || "en"]} onPress={() => setIsModalOpen(3)} />
       </View>
       <ItemCenter>
-        {squadPreset?.map((item, i) => (
+        {squadPreset?.map((item: squadPresetProps, i: string) => (
           <SlabBlock key={i} place={item.presetName} onPress={() => handlePress(item)} />
         ))}
       </ItemCenter>
