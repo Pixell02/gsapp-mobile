b6cb569fdc4fd28124d8658b04e5447cd034f7aa
Pixel <nikodems1308@gmail.com>
2023-10-10 15:29:26 +0200

    create custom hook to useContext

---
+++ screens/OpponentsScreen/hooks/useDataContext.tsx
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import { DataContext } from '../context/DataContext';
+
+const useDataContext = () => {
+
+    const context = useContext(DataContext);
+    if(!context) {
+        throw Error ("DataContext");
+    }
+
+  return context;
+}
+
+export default useDataContext
