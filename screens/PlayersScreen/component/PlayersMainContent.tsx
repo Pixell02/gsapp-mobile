0f56a52185ce522a70d03fb3ee96d75d440893d0
Pixel <nikodems1308@gmail.com>
2023-10-10 11:04:47 +0200

    reduce code

--- screens/PlayersScreen/component/PlayersMainContent.tsx
+++ screens/PlayersScreen/component/PlayersMainContent.tsx
@@ -5,40 +5,13 @@ import MainPlayers from "./PlayersMainContent/MainPlayers";
 import SquadPreset from "./PlayersMainContent/SquadPreset";
 import TeamPlayers from "./PlayersMainContent/TeamPlayers";
 
-interface props {
-  playerData: object;
-  setPlayerData: (playerData: object) => void;
-  setIsOpen: (value: number) => void;
-  selectedValue: string;
-  setSquadData: (value) => void;
-  setSelectedValue?: (selectedValue: string) => void;
-}
-
-export default function PlayersMainContent(props: props): JSX.Element {
+export default function PlayersMainContent(): JSX.Element {
   const uid = useTeamUid();
-  console.log(uid)
   return (
     <View>
-      <MainPlayers
-        selectedValue={props.selectedValue}
-        setSelectedValue={props.setSelectedValue}
-        setPlayerData={props.setPlayerData}
-        setIsOpen={props.setIsOpen}
-      />
-
-      {uid && (
-        <TeamPlayers
-          uid={uid}
-          selectedValue={props.selectedValue}
-          setSelectedValue={props.setSelectedValue}
-          setIsOpen={props.setIsOpen}
-          setPlayerData={props.setPlayerData}
-        />
-      )}
-      <SquadPreset 
-      setIsOpen={props.setIsOpen}
-      setSquadData={props.setSquadData}
-      />
+      <MainPlayers />
+      {uid && <TeamPlayers uid={uid} />}
+      <SquadPreset />
     </View>
   );
 }
