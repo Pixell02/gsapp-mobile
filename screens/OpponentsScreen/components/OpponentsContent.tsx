7bd9c4573c97cdab3c857f69204578f89cbb6fcd
Pixel <nikodems1308@gmail.com>
2023-10-10 15:30:33 +0200

    reduce code

--- screens/OpponentsScreen/components/OpponentsContent.tsx
+++ screens/OpponentsScreen/components/OpponentsContent.tsx
@@ -1,43 +1,18 @@
 import React from "react";
 import { View } from "react-native";
-import MainOpponent from "./OpponentContent/MainOpponent";
 import useTeamUid from "../../../hooks/useTeamUid";
-import TeamOpponent from "./OpponentContent/TeamOpponent";
+import MainOpponent from "./OpponentContent/MainOpponent";
 import PlaygroundContent from "./OpponentContent/PlaygroundContent";
+import TeamOpponent from "./OpponentContent/TeamOpponent";
 
-interface props {
-  setOpponentData: (value: object) => void;
-  setSelectedValue: (value: string) => void;
-  opponentData: object;
-  selectedValue: string;
-  setIsOpen: (value: number) => void;
-  setPlace: (value: string) => void
-}
-
-export default function OpponentsContent(props: props): JSX.Element {
-
+export default function OpponentsContent(): JSX.Element {
   const uid = useTeamUid();
 
   return (
     <View>
-      <MainOpponent
-        setOpponentData={props.setOpponentData}
-        setSelectedValue={props.setSelectedValue}
-        selectedValue={props.selectedValue}
-        setIsOpen={props.setIsOpen}
-      />
-      {uid &&
-        <TeamOpponent
-          uid={uid}
-          setOpponentData={props.setOpponentData}
-          setSelectedValue={props.setSelectedValue}
-          selectedValue={props.selectedValue}
-          setIsOpen={props.setIsOpen}
-        />}
-        <PlaygroundContent 
-          setIsOpen={props.setIsOpen}
-          setPlace={props.setPlace}
-        />
+      <MainOpponent />
+      {uid && <TeamOpponent uid={uid} />}
+      <PlaygroundContent />
     </View>
   );
 }
