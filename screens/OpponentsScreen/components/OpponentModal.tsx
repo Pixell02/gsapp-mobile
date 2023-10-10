326e6e7bdca8c653dce8395e7f31c61bcbda1f15
Pixel <nikodems1308@gmail.com>
2023-10-10 15:30:23 +0200

    reduce code

--- screens/OpponentsScreen/components/OpponentModal.tsx
+++ screens/OpponentsScreen/components/OpponentModal.tsx
@@ -1,41 +1,19 @@
 import React from "react";
+import useModalContextProvider from "../../MainPanelScreen/component/hooks/useModalContextProvider";
 import AddOpponent from "./AddOpponent";
-import EditOpponent from "./EditOpponent";
 import AddPlace from "./AddPlace";
+import EditOpponent from "./EditOpponent";
 import EditPlace from "./EditPlace";
 
-interface props {
-  isOpen: number;
-  setIsOpen: () => void;
-  setOpponentData: (value) => void;
-  place: object;
-  setPlace: (value) => void;
-  opponentData: object;
-}
+export default function OpponentModal() {
+  const { isModalOpen } = useModalContextProvider();
 
-export default function OpponentModal(props: props) {
   return (
     <>
-      {props.isOpen === 1 && (
-        <AddOpponent
-          isOpen={props.isOpen}
-          setIsOpen={props.setIsOpen}
-          teamData={props.opponentData}
-          setTeamData={props.setOpponentData}
-        />
-      )}
-      {props.isOpen === 2 && (
-        <EditOpponent
-          isOpen={props.isOpen}
-          setIsOpen={props.setIsOpen}
-          teamData={props.opponentData}
-          setTeamData={props.setOpponentData}
-        />
-      )}
-      {props.isOpen === 3 && <AddPlace isOpen={props.isOpen} setIsOpen={props.setIsOpen} setData={props.isOpen} />}
-      {props.isOpen === 4 && (
-        <EditPlace isOpen={props.isOpen} setIsOpen={props.setIsOpen} setData={props.setPlace} data={props.place} />
-      )}
+      {isModalOpen === 1 && <AddOpponent />}
+      {isModalOpen === 2 && <EditOpponent />}
+      {isModalOpen === 3 && <AddPlace />}
+      {isModalOpen === 4 && <EditPlace />}
     </>
   );
 }
